43164c7ba8f69947a2a8047933ba2776d4aeda2d
Tomas <norkunas.tom@gmail.com>
2015-07-06 16:42:46 +0300

    main class test file

---
+++ tests/OneSignalTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OneSignal\Tests;
+
+use OneSignal\OneSignal;
+
+/**
+ * @covers OneSignal
+ */
+class OneSignalTest extends \PHPUnit_Framework_TestCase
+{
+    private $api;
+
+    public function setUp()
+    {
+        $this->api = new OneSignal();
+    }
+
+    /**
+     * @covers OneSignal::__get
+     */
+    public function testInstances()
+    {
+        $this->assertInstanceOf('OneSignal\Apps', $this->api->apps);
+        $this->assertInstanceOf('OneSignal\Devices', $this->api->devices);
+        $this->assertInstanceOf('OneSignal\Notifications', $this->api->notifications);
+    }
+
+    /**
+     * @expectedException \PHPUnit_Framework_Error
+     */
+    public function testBadInstance()
+    {
+        $this->api->unknownInstance;
+    }
+
+    public function testConfig()
+    {
+        $this->assertInstanceOf('OneSignal\Config', $this->api->getConfig());
+    }
+
+    public function testClient()
+    {
+        $this->assertInstanceOf('GuzzleHttp\Client', $this->api->getClient());
+    }
+}
