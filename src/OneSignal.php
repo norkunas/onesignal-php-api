c185e4a3fa2c07c4edfcb10a1d671200b79f8978
Tomas <norkunas.tom@gmail.com>
2016-05-09 15:17:23 +0300

    Catch php7 throwable in request

--- src/OneSignal.php
+++ src/OneSignal.php
@@ -100,9 +100,11 @@ class OneSignal
     public function request($method, $uri, array $headers = [], $body = null)
     {
         try {
-            $response = $this->client->send($method, self::API_URL.$uri, $headers, $body);
+            $response = $this->client->send($method, self::API_URL . $uri, $headers, $body);
 
             return json_decode($response->getBody(), true);
+        } catch (\Throwable $t) {
+            throw new OneSignalException($t->getMessage());
         } catch (\Exception $e) {
             throw new OneSignalException($e->getMessage());
         }
