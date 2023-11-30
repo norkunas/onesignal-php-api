87924c672fc9e39464beb5246275e256a3ca9b97
Ricardo Cerljenko <ricardo@lloyds-digital.com>
2023-11-30 09:39:37 +0100

    introduce dto

--- src/Dto/Segments/CreateSegment.php
+++ src/Dto/Segments/CreateSegment.php
@@ -31,7 +31,7 @@ class CreateSegment implements AbstractDto
     /**
      * @param array<int, array> $filters
      */
-    public function setFilters(array $filters): self
+    public function setFilters(array $filters = []): self
     {
         $this->filters = $filters;
 
