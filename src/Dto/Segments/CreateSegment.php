ec753de0c971de8e55e96fb5607ae6035cc5a07d
Ricardo Cerljenko <ricardo@lloyds-digital.com>
2023-11-30 11:47:18 +0100

    removed []

--- src/Dto/Segments/CreateSegment.php
+++ src/Dto/Segments/CreateSegment.php
@@ -41,7 +41,7 @@ class CreateSegment implements AbstractDto
     /**
      * @param array<int, array> $filters
      */
-    public function setFilters(array $filters = []): self
+    public function setFilters(array $filters): self
     {
         $this->filters = $filters;
 
