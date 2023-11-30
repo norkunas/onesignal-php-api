21b93b92c594b284dac4db71d17c30ac84f33ec4
Ricardo Cerljenko <ricardo@lloyds-digital.com>
2023-11-30 09:06:07 +0100

    fix errors

--- src/Segments.php
+++ src/Segments.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace OneSignal;
 
-use OneSignal\Resolver\ResolverFactory;
-
 class Segments extends AbstractApi
 {
     public function __construct(OneSignal $client)
