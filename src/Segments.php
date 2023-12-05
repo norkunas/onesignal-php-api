2a919711a12c2661c44ec61fe3579c05a6fe862b
Ricardo Cerljenko <ricardo@lloyds-digital.com>
2023-12-05 13:06:21 +0100

    added separate makeRequest method

--- src/Segments.php
+++ src/Segments.php
@@ -29,7 +29,7 @@ class Segments extends AbstractApi
         $request = $this->createRequest('GET', '/apps/'.$appId.'/segments?'.http_build_query($listSegmentsDto->toArray()));
         $request = $request->withHeader('Authorization', "Basic {$this->client->getConfig()->getApplicationAuthKey()}");
 
-        return ListSegmentsResponse::makeFromResponse($this->client->sendRequest($request));
+        return ListSegmentsResponse::makeFromResponse($this->client->makeRequest($request));
     }
 
     /**
@@ -46,7 +46,7 @@ class Segments extends AbstractApi
         $request = $request->withHeader('Content-Type', 'application/json');
         $request = $request->withBody($this->createStream($createSegmentDto->toArray()));
 
-        return CreateSegmentResponse::makeFromResponse($this->client->sendRequest($request));
+        return CreateSegmentResponse::makeFromResponse($this->client->makeRequest($request));
     }
 
     /**
@@ -63,6 +63,6 @@ class Segments extends AbstractApi
         $request = $this->createRequest('DELETE', '/apps/'.$appId.'/segments/'.$id);
         $request = $request->withHeader('Authorization', "Basic {$this->client->getConfig()->getApplicationAuthKey()}");
 
-        return DeleteSegmentResponse::makeFromResponse($this->client->sendRequest($request));
+        return DeleteSegmentResponse::makeFromResponse($this->client->makeRequest($request));
     }
 }
